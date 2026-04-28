cc3695e08da42aa4fcedc136165be27b29e9b26c
harry6396 <harrytyagi2371@gmail.com>
2026-04-28 23:54:28 +0530

    feat: add service-external template and update build script to package service chart

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ helm package deplorch/secret -d .
 helm package deplorch/configmap -d .
 helm package deplorch/namespace -d .
 helm package deplorch/deployment -d .
+helm package deplorch/service -d .
 
 export REPO_URL="https://swadeshitech.github.io/base-helm-charts"
 helm repo index . --url $REPO_URL
