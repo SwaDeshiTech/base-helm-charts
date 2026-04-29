67169fb557c64fa8a74ef5f920286619acbc930d
harry6396 <harrytyagi2371@gmail.com>
2026-04-30 01:34:59 +0530

    feat: add ingress chart to helm package and enable json parsing for ingress annotations

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ helm package deplorch/namespace -d .
 helm package deplorch/deployment -d .
 helm package deplorch/service -d .
 helm package deplorch/service-external -d .
+helm package deplorch/ingress -d .
 
 export REPO_URL="https://swadeshitech.github.io/base-helm-charts"
 helm repo index . --url $REPO_URL
