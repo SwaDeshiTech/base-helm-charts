2695c7c224e4a9da5d0a94e98b62ea25d3779796
harry6396 <harrytyagi2371@gmail.com>
2026-08-20 22:38:04 +0530

    feat: add service-account Helm chart and update index.yaml

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ helm package deplorch/service-external -d .
 helm package deplorch/ingress -d .
 helm package deplorch/ephemeral-service -d .
 helm package deplorch/environment-orchestrator -d .
+helm package deplorch/service-account -d .
 
 export REPO_URL="https://swadeshitech.github.io/base-helm-charts"
 helm repo index . --url $REPO_URL
